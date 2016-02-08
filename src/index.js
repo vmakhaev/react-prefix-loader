0eada5d22b4ece198441089eabb026157f1776a7
Vladimir Makhaev <vmakhaev@gmail.com>
2016-02-08 13:48:21 -0500

    ignore options

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 const exportDefaultContainerRegex = /export default .*\(([a-zA-Z]*)/
 const exportDefaultRegex = /export default ([a-zA-Z]*)/
 const classNameRegex = /className=\'([a-zA-Z\-\s]*)\'/g
-const needPrefixRegex = /^[a-z]/
-
-function prefix (className, name) {
-  if (needPrefixRegex.test(className)) return `${name}-${className}`
-
-  return className
-}
 
 function loader (source, inputSourceMap) {
   let matches = exportDefaultContainerRegex.exec(source) ||
@@ -19,7 +12,11 @@ function loader (source, inputSourceMap) {
     source = source.replace(classNameRegex, (text, classNames) => {
       let prefixedClassNames = classNames
         .split(' ')
-        .map((className) => prefix(className, name))
+        .map((className) => {
+          if (ignoreClassName(className, this.options.reactPrefixLoader)) return className
+
+          return `${name}-${className}`
+        })
         .join(' ')
 
       return `className='${prefixedClassNames}'`
@@ -27,6 +24,29 @@ function loader (source, inputSourceMap) {
   }
 
   this.callback(null, source, inputSourceMap)
+}
+
+function ignoreClassName (className, options = {}) {
+  return classMatchesTest(className, options.ignore) ||
+    className.trim().length === 0 || /^[A-Z-]/.test(className)
+}
+
+function classMatchesTest (className, ignore) {
+  if (!ignore) return false
+
+  className = className.trim()
+
+  if (ignore instanceof RegExp) return ignore.exec(className)
+
+  if (Array.isArray(ignore)) {
+    return ignore.some((test) => {
+      if (test instanceof RegExp) return test.exec(className)
+
+      return className === test
+    })
+  }
+
+  return className === ignore
 }
 
 export default loader
